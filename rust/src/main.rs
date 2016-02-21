4a001ee77eadd53facc30b3fe4383903b1857f97
kyle merfeld <kmerfeld1@gmail.com>
2016-02-20 18:19:39 -0600

    fixed up game. rust version should be done now

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,5 +1,7 @@
 use std::io;
-//use std::thread::sleep;
+use std::thread;
+use std::time::Duration;
+
 
 fn clear_screen() {
     for _ in 0..50 {
@@ -13,6 +15,9 @@ fn pause() {
 }
 
 fn main() {
+    let one_seconds = Duration::new(1, 0);
+    let two_seconds = Duration::new(2, 0);
+    //let three_seconds = Duration::new(3, 0);
 
     println!("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
     println!("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
@@ -34,7 +39,6 @@ fn main() {
         println!("     (\\__/)");
         println!("     ( -.-)");
         println!("    C(\")(\")");
-        //sleep(2);
         println!("What do you want to do with it?");
         println!(" 1) Pet");
         println!(" 2) Poke");
@@ -54,22 +58,22 @@ fn main() {
         clear_screen();
 
 
-        //let choice = 1;
         if choice == 1{
             println!("     (\\__/) Bunny like pets. Now let me sleep.");
             println!("     ( -.-) That means leave.                 ");
             println!("    C(\")(\")                                   ");
-            //time.sleep(5);
+            thread::sleep(two_seconds);
+            clear_screen();
             println!(" umm, I think its best to do what it says.... ");
             println!("\nPress enter to continue ...");
             pause();
+            clear_screen();
         }
         else if choice == 2{
-            println!("choice 2");
             println!("     (\\__/)");
             println!("     ( -.-)");
             println!("    C(\")(\")");
-            //time.sleep(3);
+            thread::sleep(two_seconds);
             clear_screen();
             println!("     (\\__/)");
             println!("     ( 0.0)");
@@ -77,37 +81,42 @@ fn main() {
             println!(" WHO STOLE MY CARROTS???");
             println!(" I WILL DESTROY THE WOLRD");
             println!(" MWAHAHA");
-            //time.sleep(2.5);
+            thread::sleep(two_seconds);
             let v: Vec<i32> = vec![10,9,8,7,6,5,4,3,2,1,0];
-            for i in 0..10 {
+            for i in v {
                 clear_screen();
                 println!("{}", i);
+                thread::sleep(one_seconds);
             }
+            clear_screen();
             println!("SOMEONE POKED THE BUNNY. THE WORLD IS OVER.");
             println!("Guess the bunny beat those terrorists to it...");
             pause();
+            clear_screen();
             break;
 
 
         }
-        else if choice == 3{
-            println!("choice 3");
+
+
+        else if choice == 3 {
+            thread::sleep(two_seconds);
             println!("     (\\__/)");
             println!("     ( -.-)");
             println!("    C(\")(\")");
-            //time.sleep(1.5);
+            thread::sleep(two_seconds);
             println!(" random joke  im to lazy to think of");
-            //time.sleep(3);
+            thread::sleep(two_seconds);
             clear_screen();
             println!("     (\\__/) gasp");
             println!("     ( 0.0)");
             println!("    C(\")(\")");
-            //time.sleep(3);
+            thread::sleep(two_seconds);
             clear_screen();
             println!("     (\\__/) Giggle, lol");
             println!("     ( *.*)");
             println!("    C(\")(\")");
-            //time.sleep(3);
+            thread::sleep(two_seconds);
             clear_screen();
             println!(" You made the bunny laugh, awwww cute.");
             println!("\n Press enter to continue ...");
@@ -117,31 +126,30 @@ fn main() {
 
         }
         else if choice == 4{
-            println!("choice 5");
             println!("     (\\__/)");
             println!("     ( -.-)");
             println!("    C(\")(\")");
             println!(" hey bunny");
-            //time.sleep(3);
+            thread::sleep(two_seconds);
             clear_screen();
             println!("     (\\__/) ?");
             println!("     ( o.o)");
             println!("    C(\")(\")");
-            //time.sleep(3);
+            thread::sleep(two_seconds);
             clear_screen();
             println!("     (\\__/) ?");
             println!("     ( o o)");
             println!("    C(\")(\")");
             println!(" I got your nose");
-            //time.sleep(3);
+            thread::sleep(two_seconds);
             clear_screen();
             println!("     (\\__/) AHHHHH!!");
             println!("     ( o o)");
             println!("    C(\")(\")");
-            //time.sleep(4);
+            thread::sleep(two_seconds);
             clear_screen();
             println!("You stole the bunny's nose. why, why did you do that??? \nWhy??");
-            //time.sleep(3)
+            thread::sleep(two_seconds);
             println!("\n Press enter");
             clear_screen();
             break;
@@ -160,15 +168,16 @@ fn main() {
 
                 let answer = String::from("bunny");
                 if name == answer {
+                    clear_screen();
                     println!("Congratulations! you have won the game!");
                     println!("When i first made the game a song played at this part");
                     println!("I did not include it for copyright reasons. Thanks for playing");
-                    println!("\n Press enter");
+                    println!("\nPress enter");
                     pause();
-                    break;
+                    return;
                 }
                 else {
-
+                    clear_screen();
                     println!("EXTERMANATE!!!, wait sorry WRONG USER")
 
                 }
